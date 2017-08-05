8bbf1cd4d547f5ba506cd64e7e74f05b68a4317b
ellismiranda <Ellis@MacBook-Pro.local>
2017-08-04 18:22:22 -0700

    [Feature] LookupCommand: function will cycle through the current processes and check if it's the one you're looking for. Then do something based on that process

--- handlers/processes.js
+++ handlers/processes.js
@@ -2,15 +2,31 @@ const ps = require('ps-node');
 
 //will look up all processes currently happening
 
-ps.lookup({
+// ps.lookup({
+//
+// }, function(err, resultList) {
+//   if (err) {
+//     throw new Error(err);
+//   }
+//   resultList.forEach(function(process) {
+//     if (process.command === '/Applications/Google') {
+//       console.log("Found one!");
+//     }
+//   })
+// })
 
-}, function(err, resultList) {
-  if (err) {
-    throw new Error(err);
-  }
-  resultList.forEach(function(process) {
-    if (process.command === '/Applications/Google') {
-      console.log("Found one!");
-    }
+function lookupCommand(command, callback) {
+  let found = false;
+  return ps.lookup({ }, (err, resultList) => {
+    resultList.forEach((process) => {
+      if (process.command === command && !found) {
+        console.log("Found one!");
+        callback();
+      }
+    })
   })
-})
+}
+
+module.exports = {
+  lookupCommand
+}
