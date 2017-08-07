87924bf6213478d412908c94a4acfb6ac2b15236
ellismiranda <Ellis@MacBook-Pro.Caledonia>
2017-08-07 10:49:37 -0700

    Added new execution for a list of file names stored in /Documents

--- handlers/execution.js
+++ handlers/execution.js
@@ -1,7 +1,7 @@
-var util = require('util')
 var exec = require('child_process').exec;
+var path = require('path');
 
-function puts(error, stdout, stderr) { util.puts(stdout) }
+function puts(error, stdout, stderr) { console.log(stdout) }
 
 function execute(command) {
   exec(command, puts);
@@ -12,7 +12,15 @@ function open(path) {
   execute(cmd);
 }
 
+function openManyFiles(files) {
+  files.forEach((file) => {
+    const p = path.join(__dirname + '/../Documents/') + file;
+    open(p);
+  })
+}
+
 module.exports = {
   execute,
-  open
+  open,
+  openManyFiles
 }
