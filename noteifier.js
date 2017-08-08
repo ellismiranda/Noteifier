98a1f9e86245ff9f4882964811fca3f8f710d35c
ellismiranda <Ellis@MacBook-Pro.Caledonia>
2017-08-07 18:32:22 -0700

    [Add] Multiple Applications: Added a function to send notifications for multiple applications but that doesn't make much sense so it's commented out

--- noteifier.js
+++ noteifier.js
@@ -9,23 +9,39 @@ const { DESIRED, appToKeyword } = require('./models/constants');
 const pathToDocs = __dirname + '/Documents/';
 
 /*
-[O] for each of the processes in the list
+[X] for each of the processes in the list
 [X]   check processes
 [X]   if the processes you're looking for are newly opened*
 [X]     read through the files to check for keywords
 [X]     if the file has the keyword add it to the list of files
-[O]       if there are any files, send the notification
-[O]         if the notification is clicked
-[O]           open the files
+[X]       if there are any files, send the notification
+[X]         if the notification is clicked
+[X]           open the files
 
 FOR A SINGLE PROCESS:
  *checks if they're currently open
 */
 
-lookupCommand(DESIRED[0], function() {
-  const files = checkFiles(file => contains(file, appToKeyword(DESIRED[0])));
-  console.log(files);
-  if (files) {
-    sendNotification('click on me!', 'I have some files for you!', files, openManyFiles);
-  }
-})
+function singleApplication() {
+  lookupCommand(DESIRED[0], function() {
+    const files = checkFiles(file => contains(file, appToKeyword(DESIRED[0])));
+    console.log(files);
+    if (files) {
+      sendNotification('click on me!', 'I have some files for you!', files, openManyFiles);
+    }
+  })
+}
+
+// function multipleApplications() {
+//   DESIRED.forEach((application) => {
+//     lookupCommand(DESIRED[0], function() {
+//       const files = checkFiles(file => contains(file, appToKeyword(DESIRED[0])));
+//       console.log(files);
+//       if (files) {
+//         sendNotification('click on me!', 'I have some files for you!', files, openManyFiles);
+//       }
+//     })
+//   })
+// }
+
+multipleApplications();
