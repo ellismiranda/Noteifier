dbbbf702b24ad66336e977fd51e3df6eadc46af5
ellismiranda <Ellis@MacBook-Pro-2.local>
2018-03-07 13:30:55 -0500

    [remove] got rid of unused code

--- noteifier.py
+++ noteifier.py
@@ -1,31 +1,8 @@
 from models.constants import monitored_applications
 from handlers.processes import lookup_process, get_active_processes
 from handlers.tools import app_to_name, app_to_keyword, check_files, generate_open_files_command, notify
-from twisted.internet import task, reactor
 import threading
 import time
-
-accounted_for = [p for p in get_active_processes() if p in monitored_applications]
-
-
-def noteifier():
-    while True: # comment out for wrapper. ALSO REMOVE THIS AND MAKE A THREADABLE CLASS
-        for application in monitored_applications:
-            if lookup_process(application):
-                if application not in accounted_for:
-                    name = app_to_name(application)
-                    files = check_files(app_to_keyword(application))
-                    if files:
-                        cmd = generate_open_files_command(files)
-                        time.sleep(2)
-                        notify(message='Click on me to open notes about {}'.format(name), title='Noteifier', command=cmd)
-                        accounted_for.append(application)
-
-
-def wrapper(fun):
-    looper = task.LoopingCall(fun)
-    looper.start(5)
-    reactor.run()
 
 
 class Noteifier():
