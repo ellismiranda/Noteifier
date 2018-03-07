36c2e600de897cafb32d8d6378dfa1e3a642d868
ellismiranda <Ellis@MacBook-Pro-2.local>
2018-03-06 19:10:31 -0500

    [add] created new Noteifier object class based on the previous function. Will see if threading works with this

--- noteifier.py
+++ noteifier.py
@@ -2,6 +2,7 @@ from models.constants import monitored_applications
 from handlers.processes import lookup_process, get_active_processes
 from handlers.tools import app_to_name, app_to_keyword, check_files, generate_open_files_command, notify
 from twisted.internet import task, reactor
+import threading
 import time
 
 accounted_for = [p for p in get_active_processes() if p in monitored_applications]
@@ -25,3 +26,27 @@ def wrapper(fun):
     looper = task.LoopingCall(fun)
     looper.start(5)
     reactor.run()
+
+
+class Noteifier():
+    def __init__(self):
+        self.paused = threading.Event().clear()
+
+    def run(self):
+        while True:
+            if not self.paused.is_set():
+                for application in monitored_applications:
+                    if lookup_process(application):
+                        if application not in accounted_for:
+                            files = check_files(app_to_keyword(application))
+                            if files:
+                                cmd = generate_open_files_command(files)
+                                time.sleep(2)
+                                notify(message='Click on me to open notes about {}'.format(app_to_name(application)), title='Noteifier', command=cmd)
+                                accounted_for.append(application)
+
+    def pause(self):
+        self.paused.set()
+
+    def resume(self):
+        self.paused.clear()
