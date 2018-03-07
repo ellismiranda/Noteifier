80378e51c312095f56f26dbf035d59904ced2588
ellismiranda <Ellis@MacBook-Pro-2.local>
2018-03-07 13:31:27 -0500

    [change] the running status changed slightly; new button for creating notes (for futre)

--- test_noteifier.py
+++ test_noteifier.py
@@ -7,16 +7,20 @@ class TestMenuBarApp(rumps.App):
     def __init__(self):
         super(TestMenuBarApp, self).__init__("Test App")
         self.icon = 'assets/icon.png'
-        self.menu = ['RUNNING',]
+        self.menu = ['Status: RUNNING', 'New Note']
         self.noteifier = Noteifier()
         self.monitor = Thread(target=self.noteifier.run,)
         self.monitor.start()
 
-    @rumps.clicked('RUNNING')
+    @rumps.clicked('Status: RUNNING')
     def running(self, sender):
         self.noteifier.resume() if sender.title == 'PAUSED' else self.noteifier.pause()
-        sender.title = 'RUNNING' if sender.title == 'PAUSED' else 'PAUSED'
-        
+        sender.title = 'Status: RUNNING' if sender.title == 'Status: PAUSED' else 'Status: PAUSED'
+
+    # @rumps.clicked('New Note')
+    # def button(self):
+    #     new_note = rumps.Window("New Note").run()
+
 
 if __name__ == '__main__':
     TestMenuBarApp().run()
