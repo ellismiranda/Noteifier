dc2423cd19283058b0c2a04a1539c0058de45cd9
ellismiranda <Ellis@MacBook-Pro-2.local>
2018-04-11 11:18:06 -0400

    [add] temporary re-hash of monitored_applications list

--- models/constants.py
+++ models/constants.py
@@ -3,3 +3,9 @@ monitored_applications = {
     '/Applications/RuneScape.app/Contents/MacOS/RuneScape': ('RuneScape', '[rs]'),
     '/Applications/Spotify.app/Contents/MacOS/Spotify': ('Spotify', '[sfy]'),
 }
+
+monitored_applications2 = {
+    'Google Chrome': ['/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'],
+    'RuneScape': ['/Applications/RuneScape.app/Contents/MacOS/RuneScape'],
+    'Spotify': ['/Applications/Spotify.app/Contents/MacOS/Spotify']
+}
